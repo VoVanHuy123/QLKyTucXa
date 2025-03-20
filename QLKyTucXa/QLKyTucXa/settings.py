5a5e887b2b5518b945b5d53d250741987e90e27f
VoVanHuy123 <vovanhuy2k4@gmail.com>
2025-03-20 22:57:08 +0700

    test deploy 18

--- QLKyTucXa/QLKyTucXa/settings.py
+++ QLKyTucXa/QLKyTucXa/settings.py
@@ -116,7 +116,7 @@ WSGI_APPLICATION = 'QLKyTucXa.wsgi.application'
 # }
 
 
-# cách 222222222
+# cách 22222222222222
 import socket
 hostname = socket.gethostname()
 if "pythonanywhere" in hostname:  # Nếu chạy trên PythonAnywhere
