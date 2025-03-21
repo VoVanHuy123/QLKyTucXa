1d0fab5f1faecd2a8207a2570dadc13d2de305c7
VoVanHuy123 <vovanhuy2k4@gmail.com>
2025-03-21 22:02:14 +0700

    1

--- QLKyTucXa/KyTucXa/urls.py
+++ QLKyTucXa/KyTucXa/urls.py
@@ -3,13 +3,11 @@ from django.urls import path, include
 from . import views
 # from courses.admin import admin_site
 from rest_framework.routers import DefaultRouter
-from .update_code import update_code_and_deploy
 
 routes = DefaultRouter()
 routes.register('rooms', views.RoomViewSet, basename='room')
 
 urlpatterns = [
     path('', include(routes.urls)),
-    path("update-code/", update_code_and_deploy, name="update_code"),
 
 ]
