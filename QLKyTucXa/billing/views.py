8cf4e55fc01272106281cb5293f2d1d5dce71be4
VoVanHuy123 <vovanhuy2k4@gmail.com>
2025-04-16 20:50:07 +0700

    sua billing

--- QLKyTucXa/billing/views.py
+++ QLKyTucXa/billing/views.py
@@ -7,17 +7,21 @@ from billing.serializers import InvoiceSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from .filter import InvoicesFilter
 from .paginators import InvoicePaginater
+from .perms import IsAdminOrUserInvoices
 
 
 # Create your views here.
-class InvoiceViewSet(viewsets.ViewSet,generics.CreateAPIView,generics.RetrieveAPIView,generics.ListAPIView):
+class InvoiceViewSet(viewsets.ViewSet,generics.CreateAPIView,
+                     generics.RetrieveAPIView,generics.ListAPIView,
+                     generics.UpdateAPIView,generics.DestroyAPIView ):
     queryset = Invoice.objects.filter(active = True)
     serializer_class = InvoiceSerializer
     pagination_class = InvoicePaginater
     filter_backends = [DjangoFilterBackend]
     filterset_class = InvoicesFilter
+    permission_classes = [IsAdminOrUserInvoices]
     
-    def get_permissions(self):
-        if self.request.method == 'POST':
-            return [permissions.IsAdminUser()]  # Chỉ Admin mới được tạo
-        return [permissions.IsAuthenticated()]  # Còn lại chỉ cần đăng nhập
+    # def get_permissions(self):
+    #     if self.request.method == 'POST':
+    #         return [permissions.IsAdminUser()]  # Chỉ Admin mới được tạo
+    #     return [permissions.IsAuthenticated()]  # Còn lại chỉ cần đăng nhập
